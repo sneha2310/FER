de9b03612b78c2086d0466dde53568a20f18761c
Sneha <67871362+sneha2310@users.noreply.github.com>
2022-09-20 18:14:42 +0530

    Update settings.py

--- face/settings.py
+++ face/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y=b!nb&ncp!#4cfc^m7-j&3r@&%(@-%rj49eee6v$atvk%gv&6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
